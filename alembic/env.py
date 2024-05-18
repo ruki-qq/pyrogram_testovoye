c21db4aa78fed1bb61f39e9f478a064496fa608d
ruki-qq <ruki@duck.com>
2024-05-18 13:41:56 +0300

    Setup alembic

--- alembic/env.py
+++ alembic/env.py
@@ -7,25 +7,21 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from bot.database import db_config
+from bot.database.models import Base
+
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = Base.metadata
+
+
+config.set_main_option("sqlalchemy.url", db_config.url)
 
 
 def run_migrations_offline() -> None:
