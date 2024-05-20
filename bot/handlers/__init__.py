2566dd9347be761418719a9fdce5b8788cc17558
ruki-qq <ruki@duck.com>
2024-05-20 06:28:43 +0300

    Add function to check profiles for msg send

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,7 @@
+__all__ = (
+    "msg_handler",
+    "trigger_handler",
+)
+
+from .get_msg import msg_handler
+from .get_triggers import trigger_handler
---
+++ bot/utils/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ("check_profiles_and_send_msg",)
+
+from .check_profiles_and_send_msg import check_profiles_and_send_msg
